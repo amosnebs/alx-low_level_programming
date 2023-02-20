3966bd4d8456a24460c0357859dd1b318428de89
amosnebs <amosfanta@gmail.com>
2023-02-20 12:24:16 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,17 @@ int main(void)
 
 	for (tens - '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
-		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
 		{
 			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*adds comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
