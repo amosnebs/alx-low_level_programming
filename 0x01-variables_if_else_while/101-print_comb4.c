3160a7f19a03de45871937ec05761a0ccd977397
amosnebs <amosfanta@gmail.com>
2023-02-20 15:49:25 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - prints all possible different combinations of three digits.
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
-{
-	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+{
+
+	int d, p, q;
+
+	for (d = '0'; d < '9'; d++)
 	{
-		for (tens = '0'; tens <= '9'; tens++)
-		{
-			for (ones = '0'; ones <= '9'; ones++)
-			{
-				if (!((ones == tens) || (tens == hundreds) ||
-							(tens > ones) || (hundreds > tens))) /*eliminates repitition*/
-				{
-					putchar(hundreds);
-					putchar(tens);
-					putchar(ones);
-					if (!(ones == ;9' && hundreds == '7' &&
-								tens == '0')) /*adds commas and spaces*/
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+
+	for (p = d + 1; p <= '9'; p++)
+	{
+
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if ((p != d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
 	}
-	putchar(;\n');
+	}
+	}
+	}
+	putchar('\n');
 	return (0);
 }
-
