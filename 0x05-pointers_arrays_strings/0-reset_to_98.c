a84c2411b0416b67eff22bdcccdc042d37a2bc62
amosnebs <amosfanta@gmail.com>
2023-02-27 09:17:49 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 
 
 /**
- * reset_to_98n - take a pointer to an int as parameter and updates
+ * reset_to_98 - take a pointer to an int as parameter and updates
  * the value it points to 98
  * @n: point to an int
- * Return: Nothing
+ * Return: Success
  */
 
 void reset_to_98(int *n)
