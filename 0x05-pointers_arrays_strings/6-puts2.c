dfd42edd5162dbcb9ea0a583a108244165ee5dbf
amosnebs <amosfanta@gmail.com>
2023-02-27 10:00:34 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * puts2 - prints one character out of two
- * @str: input
- * Return: print
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
  */
-void puts2(char *str);
+void puts2(char *str)
 {
-	int i = 0;
+	int len, i;
 
-	for (; str[i] != '\0'; i++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		if ((i % 2) == 0)
-			_putchar(str[i]);
-		else
-			continue;
+		len++;
 	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
