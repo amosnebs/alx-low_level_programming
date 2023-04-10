dc417940e70174a7284a2e2482ba044a48fa46b3
amosnebs <amosfanta@gmail.com>
2023-04-10 05:45:26 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,20 @@
-#ifndef LISTS
-#define LISTS
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _LISTS
+#define _S_LINK_LISTS_
 
 /**
+  * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-singly linked list node structure for Holberton project */ typedef struct listint_s { int n; struct listint_s *next; } listint_t;
-/**
-
-singly linked list of pointers */ typedef struct listp_s { void *p; struct listp_s *next; } listp_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -23,7 +28,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
